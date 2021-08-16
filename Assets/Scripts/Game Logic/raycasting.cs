0897db332f3a1d90bb113602b83044944a2a6aff
Sloth-Kun <slothkun00@gmail.com>
2021-08-16 18:45:09 +0200

    Raycasting proto multidirectionnal
    Update du prototype en ajoutant le check dans toutes les 6 directions voulues par les specs du jeu

--- Assets/Scripts/Game Logic/raycasting.cs
+++ Assets/Scripts/Game Logic/raycasting.cs
@@ -7,7 +7,7 @@ public class raycasting : MonoBehaviour
     public float rayLength;
     public string ballColor;
     public string colorCheck;
-    public Ray[] rays = new Ray[6];
+    private Ray[] rays = new Ray[6];
 
 
     void Start() 
@@ -20,7 +20,7 @@ public class raycasting : MonoBehaviour
         rays[4] = new Ray(transform.position, (Vector3.left + Vector3.down).normalized);
         rays[5] = new Ray(transform.position, (Vector3.right + Vector3.down).normalized);
 
-        rayLength = 50;
+        rayLength = 50; // A definir quand on aura la taille finale des boules
     }
 
     // Update is called once per frame
@@ -28,12 +28,14 @@ public class raycasting : MonoBehaviour
     {  
         colorCheck = ballColor; // Put that in start after testing
         RaycastHit hit;
-
-        if (Physics.Raycast(rays[0], out hit, rayLength)) {
-            string hitColor = hit.transform.gameObject.GetComponent<raycasting>().ballColor;
-            if (ballColor == hitColor) {
-                Vector3 hitPos = hit.transform.position - transform.position;
-                Debug.DrawRay(transform.position, hitPos, Color.green);
+        
+        foreach (Ray ray in rays) {
+            if (Physics.Raycast(ray, out hit, rayLength)) {
+                string hitColor = hit.transform.gameObject.GetComponent<raycasting>().ballColor;
+                if (ballColor == hitColor) {
+                    Vector3 hitPos = hit.transform.position - transform.position;
+                    Debug.DrawRay(transform.position, hitPos, Color.green);
+                }
             }
         }
     }
